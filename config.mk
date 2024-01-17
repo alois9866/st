4a42cbd76ffd67136c8a97611f9cd3c84329b71c
alois9866 <alois9866@gmail.com>
2024-01-18 01:45:08 +0300

    Update config.mk paths

--- config.mk
+++ config.mk
@@ -7,8 +7,8 @@ VERSION = 0.9.2
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib
 
 PKG_CONFIG = pkg-config
 
@@ -25,12 +25,3 @@ STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
 STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
 STLDFLAGS = $(LIBS) $(LDFLAGS)
 
-# OpenBSD:
-#CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
-#LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
-#       `$(PKG_CONFIG) --libs fontconfig` \
-#       `$(PKG_CONFIG) --libs freetype2`
-#MANPREFIX = ${PREFIX}/man
-
-# compiler and linker
-# CC = c99
