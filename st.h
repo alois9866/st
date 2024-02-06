74cf5d17814232dda0e409d3eab968508a916815
alois9866 <alois9866@gmail.com>
2024-02-06 22:46:29 +0300

    Apply externalpipe

--- st.h
+++ st.h
@@ -83,6 +83,8 @@ void draw(void);
 
 void kscrolldown(const Arg *);
 void kscrollup(const Arg *);
+void externalpipe(const Arg *);
+void externalpipeall(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
