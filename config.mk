f657a7cebd1024bb18ea50ba081a108ec0394fd0
Alois <alois9866@gmail.com>
2024-11-23 01:50:05 +0300

    Add optimization flags

--- config.mk
+++ config.mk
@@ -22,6 +22,6 @@ LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXrender\
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
-STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
+STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS) -O3 -march=native -g
 STLDFLAGS = $(LIBS) $(LDFLAGS)
 
